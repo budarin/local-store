6b7c2a6b24dbca5296b2b44d0ae861fcc3484641
vadim_budarin <budarin-vv@yandex.ru>
2023-11-29 23:57:32 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # local-store
+
 Local storage service
+
+## Installation
+
+```bash
+yarn add @budarin/local-store
+```
+
+## Usage
+
+```ts
+import { LocalStore } from '@budarin/local-store';
+
+const store = new LocalStore();
+
+store.set('key', 'value');
+
+store.get('key');
+
+store.remove('key');
+
+store.clear();
+```
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export class CacheStore {
+    constructor() {}
+
+    setItem(key: string, item: any): void {
+        localStorage.setItem(key, JSON.stringify(item));
+    }
+
+    getItem(key: string): any {
+        return JSON.parse(localStorage.getItem(key) as string);
+    }
+
+    removeItem(key: string): void {
+        localStorage.removeItem(key);
+    }
+
+    clear(): void {
+        localStorage.clear();
+    }
+}
